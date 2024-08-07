a08b56ae943c387f1f10749c8e74477a46b0d3be
redgaryesid <r.edgaryesid@gmail.com>
2024-08-07 17:55:39 -0500

    All changues

---
+++ functions.js
@@ -0,0 +1,9 @@
+
+function openCart(){
+    var sobre = document.getElementById("sobre");
+    sobre.classList.add("hide");
+    var home = document.getElementById("home");
+    home.classList.remove("hide")
+console.log(sobre);
+    console.log("Sii");
+}
